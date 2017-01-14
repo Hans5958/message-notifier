123fe303c7c9366a2503f26a890e4cc5e1a920b4
Hans5958 <email5958.1@gmail.com>
2017-01-14 16:25:55 +0700

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -12,41 +12,43 @@
 // ==/UserScript==
 
 // Initial things to do.
-var originalTitle = document.title
-var originalCount = 0
-var count = 0
+var originalTitle = document.title;
+var originalCount = 0;
+var count = 0;
+var player = document.createElement('audio');
+player.src = 'https://raw.githubusercontent.com/Hans5958/message-notifier/master/notificationsound.wav';
+player.preload = 'auto';
+
 
 // Creating an function for getting messages count
 // In Scratch, this is like doing a custom block.
 function getCount() {
-  var username = Scratch.INIT_DATA.LOGGED_IN_USER.model.username
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", 'https://api.scratch.mit.edu/users/' + username + '/messages/count', false);
-  xmlHttp.send(null);
-  var request = xmlHttp.responseText;
-  var parsedData = JSON.parse(request);
-  return parsedData.count;
+    var username = Scratch.INIT_DATA.LOGGED_IN_USER.model.username;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", 'https://api.scratch.mit.edu/users/' + username + '/messages/count', false);
+    xmlHttp.send(null);
+    var request = xmlHttp.responseText;
+    var parsedData = JSON.parse(request);
+    return parsedData.count;
 }
 
 // Here's the script.
 function doTask() {
-  count = getCount()
-  if (originalCount != count) {
-    if (count == 0) {
-      document.title = originalTitle
-    } else {
-      var audioElement = document.createElement('audio');
-      audioElement.setAttribute('src', ding);
-      audioElement.play();
-      document.title = "(" + count + ") New message!"
-      setTimeout(function() {
-        document.title = "(" + count + ") " + originalTitle
-      }, 2000);
+    count = getCount();
+    if (originalCount != count) {
+        if (count === 0) {
+            document.title = originalTitle;
+        } else {
+            document.title = "(" + count + ") New message!";
+            setTimeout(function() {
+                document.title = "(" + count + ") " + originalTitle;
+            }, 2000);
+            player.play();
+        }
+        originalCount = count;
     }
-    originalCount = count
-  }
 }
-doTask()
+doTask();
 setInterval(function() {
-  doTask()
+    doTask();
 }, 3000);
