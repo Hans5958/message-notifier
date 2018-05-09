f0ae9d6ca9170d4ff2170faac4bd4ad60eb92fb9
Hans5958 <email5958.1@gmail.com>
2018-05-09 21:28:53 +0700

    forgot to commit this

--- Remove Description Box/main.user.js
+++ Remove Description Box/main.user.js
@@ -8,5 +8,5 @@
 // @grant        none
 // ==/UserScript==
 
-document.getElementsByTagName("b")[0].innerHTML = "Instructions <a href=javascript:$('#description')[0].attributes.style.value='height:310px',$('.text-block')[0].remove()>❌</a>";
-document.getElementsByTagName("b")[1].innerHTML = "Notes and Credits <a href=javascript:$('#instructions')[0].attributes.style.value='height:310px',$('.tooltip-target')[0].remove()>❌</a>";
+document.getElementsByTagName("b")[0].innerHTML = "Instructions <a href=javascript:$('#description')[0].attributes.style='height:310px',$('.text-block')[0].remove()>❌</a>";
+document.getElementsByTagName("b")[1].innerHTML = "Notes and Credits <a href=javascript:$('#instructions')[0].attributes.style='height:310px',$('.tooltip-target')[0].remove()>❌</a>";
