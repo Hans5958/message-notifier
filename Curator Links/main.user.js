8d356b5f3405f79da1bb6213745b81333f017d1e
Hans5958 <email5958.1@gmail.com>
2018-05-10 10:21:00 +0700

    new userscript, curator links

---
+++ Curator Links/main.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Curator Links on Scratch
+// @namespace    scratch.mit.edu/users/Hans5958
+// @version      1
+// @description  Creates a clickable link on the curator on the front page.
+// @author       Hans5958
+// @match        http*://scratch.mit.edu/
+// @grant        none
+// ==/UserScript==
+
+var curator = document.getElementsByTagName("h4")[7].innerHTML.slice(20, 99);
+document.getElementsByTagName("h4")[7].innerHTML = "Projects Curated by <a class=\"curator-links\" href=\"https://scratch.mit.edu/users/" + curator + "\">" + curator + "</a>";
+document.getElementsByTagName("head")[0].innerHTML += "<style>a.curator-links {color: #6b6b6b; font-weight: bold;} a.curator-links:hover {text-decoration: underline;}</style>"
